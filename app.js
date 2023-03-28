697c7a12ef714ac3eebe18c59e7e6102c7408d61
levizoca <64503782+levizoca@users.noreply.github.com>
2023-03-28 11:19:02 -0300

    add email and date functions
    added email validation, turned email to unique on bd and search function using dates

--- app.js
+++ app.js
@@ -28,11 +28,49 @@ app.post('/forms_answers', (req, res) => {
     const email = req.body.email.substring(0, 150);
     const cpf = req.body.cpf.substring(0, 11);
     const phone = req.body.phone.substring(0, 11);
-    execSQLQuery(`INSERT INTO forms_answers(name, email, cpf, phone, created_at) VALUES('${name}', '${email}', '${cpf}', '${phone}', CURRENT_TIMESTAMP)`, res);
+  
+    // Validação de email
+    function validateEmail(email) {
+      var check = /\S+@\S+\.\S+/;
+      return check.test(email);
+    }
+
+    if (validateEmail(email) == false) {
+        res.json('Email inválido!');
+    } else {
+      execSQLRegister(`INSERT INTO forms_answers(name, email, cpf, phone, created_at) VALUES('${name}', '${email}', '${cpf}', '${phone}', CURRENT_TIMESTAMP)`, res);
+    }
+});
+
+// Pesquisar por data
+app.get('/search', (req, res) => {
+  const dateInit = req.body.dateInit;
+  const dateFinal = req.body.dateFinal;
+  execSQLQuery(`SELECT * FROM forms_answers WHERE created_at BETWEEN '${dateInit}' AND '${dateFinal}'` , res);
 });
 
 app.listen(port);
 console.log('API running on port ' + port);
+
+
+function execSQLRegister(sqlQry, res){
+  const connection = mysql.createConnection({
+    host     : 'localhost',
+    port     :  3306,
+    user     : 'root',
+    password : 'root',
+    database : 'newsletter'
+  });
+ 
+  connection.query(sqlQry, (error, results, fields) => {
+      if(error) 
+        res.json(error);
+      else
+        res.json('Ação realizada com sucesso!');
+      connection.end();
+      console.log('Executed!');
+  });
+}
 
 function execSQLQuery(sqlQry, res){
   const connection = mysql.createConnection({
--- database.js
+++ database.js
@@ -2,20 +2,20 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host     : 'localhost',
     port     :  3306,
-    user     : 'root',
-    password : 'root',
+    user     : 'root',  // Se necessário alterar o usuário 
+    password : 'root',  // Se necessário alterar a senha
     database : 'newsletter'
 });
 
 function createTable(conn){
     const sql = `CREATE TABLE IF NOT EXISTS forms_answers(
-                 Id serial NOT NULL AUTO_INCREMENT,
+                 Id serial NOT NULL AUTO_INCREMENT PRIMARY KEY,
                  name text NOT NULL,
-                 email text NOT NULL,
+                 email varchar(255) NOT NULL,
                  cpf text NOT NULL,
                  phone text NOT NULL,
                  created_at date NOT NULL,
-                 PRIMARY KEY (Id)
+                 UNIQUE KEY unique_email(email)
                  );`;
     
     conn.query(sql, (error, results, fields) => {
